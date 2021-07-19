8b6f9a12e93a48a798d18cce6e5c77decf1d2c89
Hyung Yeon Ji <yjhyung@ncurity.com>
2021-07-19 05:07:23 +0000

    Initial Push Go Tutorial

---
+++ A_Tour_of_Go.go
@@ -0,0 +1,13 @@
+/*Intro. A Tour of Go*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+	fmt.Println("Hello, Ncurity Platform!")
+	fmt.Println("The time is", time.Now().Local())
+}
